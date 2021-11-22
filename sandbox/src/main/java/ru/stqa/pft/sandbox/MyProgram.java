2821eaa139af317379e0cc8aa4ef11196946cc88
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-22 21:48:44 +0500

    Создание пакета

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -1,7 +1,9 @@
+package ru.stqa.pft.sandbox;
+
 public class MyProgram
 {
     public static void main (String[] args)
     {
-    System.out.println("Hello, world!!!");
+    System.out.println("Hello, world!!");
     }
 }
