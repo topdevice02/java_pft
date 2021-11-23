a54ec7e750feac1803e239e7a2f75a43fdbb36d5
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-23 20:26:45 +0500

    Создание класса Point с функцией вычисления расстояния между двумя точками на плоскости

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public static void main (String[] args) {
+    Point p1 = new Point(8,7);
+    Point p2 = new Point(-3,3);
+
+    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + distance(p1,p2));
+
+  }
+
+  public Point(double x, double y){
+      this.x = x;
+      this.y = y;
+  }
+
+  public static double distance (Point p1, Point p2){
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+  }
+}
+
+
