fd1671f13257b742e894e7ebdcf380af798b568a
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-23 20:28:33 +0500

    Замена функции на метод вычисления расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,7 +8,7 @@ public class Point {
     Point p1 = new Point(8,7);
     Point p2 = new Point(-3,3);
 
-    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + distance(p1,p2));
+    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + p1.distance(p1,p2));
 
   }
 
@@ -17,7 +17,7 @@ public class Point {
       this.y = y;
   }
 
-  public static double distance (Point p1, Point p2){
+  public double distance (Point p1, Point p2){
     return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
 }
