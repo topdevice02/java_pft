29fe33918c5fe17a0b92d8276625bc97b09926be
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-24 13:14:45 +0500

    Создание статической функции distance в запускаемом классе MyProgram и создание метода distance в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyProgram.java
@@ -2,9 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class MyProgram
 {
-    public static void main (String[] args)
-    {
-       System.out.println("Hello, world!!");
-    }
+  public static void main (String[] args) {
+    Point p1 = new Point(8,7);
+    Point p2 = new Point(-3,3);
+
+    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + distance(p1,p2));
+
+    Point p3 = new Point(10,4);
+    Point p4 = new Point(0,2);
+
+    System.out.println("Расстояние между точками (" + p3.x + "," + p3.y + ") и (" + p4.x + "," + p4.y + ") = " + p3.distance(p3,p4));
+
+  }
+
+  public static double distance (Point p1, Point p2)
+  {
+    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+  }
 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,22 +4,17 @@ public class Point {
   public double x;
   public double y;
 
-  public static void main (String[] args) {
-    Point p1 = new Point(8,7);
-    Point p2 = new Point(-3,3);
-
-    System.out.println("Расстояние между точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") = " + p1.distance(p1,p2));
-
-  }
-
   public Point(double x, double y){
       this.x = x;
       this.y = y;
   }
 
-  public double distance (Point p1, Point p2){
+  public double distance(Point p1, Point p2)
+  {
     return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
+
+
 }
 
 
