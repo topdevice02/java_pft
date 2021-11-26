1adf98df6c7f8da614c6bab7b5d42a1da232dcbe
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-26 15:54:11 +0500

    Редактирование тестов в связи с изменениями метода distance

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,13 +9,13 @@ public class PointTests {
   public void testPointFirst(){
     Point p1 = new Point(7,5);
     Point p2 = new Point(4,5);
-    Assert.assertEquals(p1.distance(p1,p2), 3.0);
+    Assert.assertEquals(p1.distance(p2), 3.0);
   }
 
   @Test
   public void testPointSecond(){
     Point p3 = new Point(-3,11);
     Point p4 = new Point(-3,7);
-    assert p3.distance(p3,p4) == 4;
+    assert p3.distance(p4) == 4;
   }
 }
