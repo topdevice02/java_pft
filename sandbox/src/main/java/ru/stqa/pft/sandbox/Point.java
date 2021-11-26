7cac9e963c47d4b6015bd13c65209da24280a1f5
topdevice02 <vyach.ivanov100@gmail.com>
2021-11-26 15:50:25 +0500

    Исправление метода distance в классе Point с одним передаваемым параметром

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -9,11 +9,10 @@ public class Point {
       this.y = y;
   }
 
-  public double distance(Point p1, Point p2)
+  public double distance(Point p2)
   {
-    return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+    return Math.sqrt((p2.x-this.x)*(p2.x-this.x)+(p2.y-this.y)*(p2.y-this.y));
   }
-
 
 }
 
