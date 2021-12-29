4960ab03b90064e44d15f6d0e64752a561c924b0
topdevice02 <vyach.ivanov100@gmail.com>
2021-12-29 20:36:38 +0500

    Реализовано автоматическое закрытие файла после записи и чтения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -55,27 +55,30 @@ public class ContactDataGenerator {
   private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
     String json = gson.toJson(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(json);
-    writer.close();
+    try (Writer writer = new FileWriter(file))
+    {
+      writer.write(json);
+    }
   }
 
   private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
     XStream xStream = new XStream();
     xStream.processAnnotations(ContactData.class);
     String xml = xStream.toXML(contacts);
-    Writer writer = new FileWriter(file);
-    writer.write(xml);
-    writer.close();
+    try (Writer writer = new FileWriter(file))
+    {
+      writer.write(xml);
+    }
   }
 
   private static void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
-    Writer writer = new FileWriter(file);
-    for(ContactData contact : contacts){
-      writer.write(String.format("%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
-              contact.getPhoto(), contact.getMobilePhone(), contact.getEmail()));
+    try (Writer writer = new FileWriter(file))
+    {
+      for(ContactData contact : contacts){
+        writer.write(String.format("%s;%s;%s;%s;%s\n", contact.getFirstname(), contact.getLastname(),
+                contact.getPhoto(), contact.getMobilePhone(), contact.getEmail()));
+      }
     }
-    writer.close();
   }
 
   private static List<ContactData> generateContacts(int count) {
