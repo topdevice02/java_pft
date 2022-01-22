7b4d778fef1b96b8a29b011375a285825638f276
topdevice02 <vyach.ivanov100@gmail.com>
2022-01-22 16:55:06 +0500

    Реализовано получение почты на встроенный в тесты почтовый сервер

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/BaseHelper.java
@@ -0,0 +1,57 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+
+public class BaseHelper {
+  protected WebDriver wd;
+  protected ApplicationManager app;
+
+  public BaseHelper(ApplicationManager app) {
+    this.app = app;
+    this.wd = app.getDriver();
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if(! text.equals(existingText)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
+  }
+
+  protected void attach(By locator, File file) {
+    if (file != null) {
+        wd.findElement(locator).sendKeys(file.getAbsolutePath());
+      }
+  }
+
+  public boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+}
--- mantis-tests/src/test/resources/config_inc.php
+++ mantis-tests/src/test/resources/config_inc.php
@@ -6,4 +6,6 @@ $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
 $g_crypto_master_salt = '97OS7ubVorTAcEMri1QlqDFhes1HznvtnGTrS26Ekj0=';
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
 
